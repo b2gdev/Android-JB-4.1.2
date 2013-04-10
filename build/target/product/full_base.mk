01c0d1e3708409b23052540c754d9cc1f3c75e6e
ruvindad <ruvindad@zone24x7.com>
2013-04-10 13:18:02 +0530

    Removed live wallpapers

--- build/target/product/full_base.mk
+++ build/target/product/full_base.mk
@@ -32,16 +32,6 @@ PRODUCT_PACKAGES := \
     VideoEditor \
     WAPPushManager
 
-PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers
-
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy \
