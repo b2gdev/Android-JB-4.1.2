59e649d170b90b2591b939f864c0b299e9fe6349
Ruvinda Dhambarage <ruvindad@zone24x7.com>
2016-01-27 11:58:00 +0530

    Added MSP430, Flasher and Camera tests

--- tcbin_misc/syscheck/syscheck_fs_build.sh
+++ tcbin_misc/syscheck/syscheck_fs_build.sh
@@ -4,15 +4,21 @@ cd ../build_scripts/
 ./backupfs_build.sh
 cd ../../
 mkdir -p out/target/product/beagleboard/recovery/root/lib/modules/2.6.37/
-cp -v tcbin_misc/syscheck/tests/syscheck out/target/product/beagleboard/recovery/root/system/bin/
-cp -v tcbin_misc/syscheck/tests/gps_test out/target/product/beagleboard/recovery/root/system/bin/
-cp -v tcbin_misc/syscheck/tests/twl3040_vib_test out/target/product/beagleboard/recovery/root/system/bin/
-cp -v tcbin_misc/syscheck/configs/fstab.omap3beagleboard out/target/product/beagleboard/recovery/root/
+cp -v tcbin_misc/syscheck/tests/syscheck out/target/product/beagleboard/recovery/root/sbin/
+cp -v tcbin_misc/syscheck/tests/gps_test out/target/product/beagleboard/recovery/root/sbin/
+cp -v tcbin_misc/syscheck/tests/twl3040_vib_test out/target/product/beagleboard/recovery/root/sbin/
+cp -v tcbin_misc/syscheck/tests/lm3553_test out/target/product/beagleboard/recovery/root/sbin/
+cp -v tcbin_misc/syscheck/configs/fstab.omap3beagleboard out/target/product/beagleboard/
 cp -v tcbin_misc/syscheck/configs/init.rc out/target/product/beagleboard/recovery/root/
-cp -v tcbin_misc/non_destructive_nandtest out/target/product/beagleboard/recovery/root/system/bin/
-cp -v tcbin_misc/destructive_nandtest out/target/product/beagleboard/recovery/root/system/bin/
+cp -v tcbin_misc/syscheck/tests/non_destructive_nandtest out/target/product/beagleboard/recovery/root/sbin/
+cp -v tcbin_misc/syscheck/tests/destructive_nandtest out/target/product/beagleboard/recovery/root/sbin/
 cp -v kernel/drivers/mtd/tests/mtd_subpagetest.ko out/target/product/beagleboard/recovery/root/usr/modules/
 cp -v kernel/drivers/mtd/tests/mtd_readtest.ko out/target/product/beagleboard/recovery/root/usr/modules/
+cp -v kernel/drivers/misc/lm3553/lm3553.ko out/target/product/beagleboard/recovery/root/usr/modules/
+mkdir -p out/target/product/beagleboard/recovery/root/system/bin/
+cp -vr out/target/product/beagleboard/system/bin/* out/target/product/beagleboard/recovery/root/system/bin/
+mkdir -p out/target/product/beagleboard/recovery/root/system/lib/
+cp -vr /home/b2g/src/b2g_dev/jb_qa/out/target/product/beagleboard/system/lib/* out/target/product/beagleboard/recovery/root/system/lib/
 cd out/target/product/beagleboard/recovery
 echo "Creating Testing filesystem"
 ../../../../../build/tools/mktarball.sh ../../../../host/linux-x86/bin/fs_get_stats root . testing testing.tar.bz2
