ebfdb9cae95c8aebe5f0f68db5703ecb78b520d9
Dave Mielke <dave@mielke.cc>
2016-11-21 16:07:45 -0500

    Trim strings before centering them. (dm)

--- bin/b2g-strings.sh
+++ bin/b2g-strings.sh
@@ -1,56 +1,102 @@
 replicateString() {
-  local variable="${1}"
-  local string="${2}"
-  local count="${3}"
+  local replicateString_variable="${1}"
+  local replicateString_string="${2}"
+  local replicateString_count="${3}"
 
-  local length=$((${#string} * count))
-  local result=""
+  local replicateString_length=$((${#replicateString_string} * replicateString_count))
+  local replicateString_result=""
 
-  while [ "${#result}" -lt "${length}" ]
+  while [ "${#replicateString_result}" -lt "${replicateString_length}" ]
   do
-    result="${result}${string}"
+    replicateString_result="${replicateString_result}${replicateString_string}"
   done
 
-  setVariable "${variable}" "${result}"
+  setVariable "${replicateString_variable}" "${replicateString_result}"
+}
+
+trimLeft() {
+  local trimLeft_variable="${1}"
+
+  local trimLeft_string
+  getVariable trimLeft_string "${trimLeft_variable}"
+  local trimLeft_changed=false
+
+  while true
+  do
+    local trimLeft_trimmed="${trimLeft_string# }"
+    [ "${trimLeft_trimmed}" = "${trimLeft_string}" ] && break
+
+    trimLeft_string="${trimLeft_trimmed}"
+    trimLeft_changed=true
+  done
+
+  ! "${trimLeft_changed}" || setVariable "${trimLeft_variable}" "${trimLeft_string}"
+}
+
+trimRight() {
+  local trimRight_variable="${1}"
+
+  local trimRight_string
+  getVariable trimRight_string "${trimRight_variable}"
+  local trimRight_changed=false
+
+  while true
+  do
+    local trimRight_trimmed="${trimRight_string% }"
+    [ "${trimRight_trimmed}" = "${trimRight_string}" ] && break
+
+    trimRight_string="${trimRight_trimmed}"
+    trimRight_changed=true
+  done
+
+  ! "${trimRight_changed}" || setVariable "${trimRight_variable}" "${trimRight_string}"
+}
+
+trimString() {
+  local trimString_variable="${1}"
+
+  trimLeft "${trimString_variable}"
+  trimRight "${trimString_variable}"
 }
 
 centerString() {
-  local variable="${1}"
-  local width="${2}"
+  local centerString_variable="${1}"
+  local centerString_width="${2}"
 
-  local string
-  getVariable string "${variable}"
-  local length="${#string}"
+  local centerString_string
+  getVariable centerString_string "${centerString_variable}"
+  trimString centerString_string
+  local centerString_length="${#centerString_string}"
 
-  if [ "${length}" -lt "${width}" ]
+  if [ "${centerString_length}" -lt "${centerString_width}" ]
   then
-    local count=$(((width - length) / 2))
+    local centerString_count=$(((centerString_width - centerString_length) / 2))
 
-    while [ "${count}" -gt 0 ]
+    while [ "${centerString_count}" -gt 0 ]
     do
-      string=" ${string}"
-      count=$((count - 1))
+      centerString_string=" ${centerString_string}"
+      centerString_count=$((centerString_count - 1))
     done
 
-    while [ "${#string}" -lt "${width}" ]
+    while [ "${#centerString_string}" -lt "${centerString_width}" ]
     do
-      string="${string} "
+      centerString_string="${centerString_string} "
     done
   else
-    local count=$(((length - width) / 2))
+    local centerString_count=$(((centerString_length - centerString_width) / 2))
 
-    while [ "${count}" -gt 0 ]
+    while [ "${centerString_count}" -gt 0 ]
     do
-      string="${string#?}"
-      count=$((count - 1))
+      centerString_string="${centerString_string#?}"
+      centerString_count=$((centerString_count - 1))
     done
 
-    while [ "${#string}" -gt "${width}" ]
+    while [ "${#centerString_string}" -gt "${centerString_width}" ]
     do
-      string="${string%?}"
+      centerString_string="${centerString_string%?}"
     done
   fi
 
-  setVariable "${variable}" "${string}"
+  setVariable "${centerString_variable}" "${centerString_string}"
 }
 
